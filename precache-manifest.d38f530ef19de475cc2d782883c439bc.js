b8f3e432aa0ddd938ad8aca27f3eae47e42ed21d
Dan Lott <danieljlott@live.com>
2019-04-20 13:33:29 -0700

    Updates

---
+++ precache-manifest.d38f530ef19de475cc2d782883c439bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6246469b97c7601d5c4",
+    "url": "/Memory-Click/static/js/runtime~main.65553346.js"
+  },
+  {
+    "revision": "cba3c578c83835ac2c7c",
+    "url": "/Memory-Click/static/js/main.793f9b4f.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Memory-Click/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "cba3c578c83835ac2c7c",
+    "url": "/Memory-Click/static/css/main.6aff5123.chunk.css"
+  },
+  {
+    "revision": "03f7b90296dc858b69c2b11aa7f7871d",
+    "url": "/Memory-Click/index.html"
+  }
+];
